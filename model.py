63304d9109bf4c36218c3a9fc9459736097184dd
Rr <roman.morning@gmail.com>
2025-07-24 06:14:45 +0300

    22

--- model.py
+++ model.py
@@ -48,7 +48,7 @@ class BCCorrectionNN(nn.Module):
 
 class DynamicsGNN(MessagePassing):
     def __init__(self, config: SimulationConfig):
-        super().__init__(aggr='add')
+        super().__init__(aggr='add', node_dim=-1)
         self.config = config
 
         node_types = ['free', 'pressure', 'velocity']
@@ -79,17 +79,17 @@ class DynamicsGNN(MessagePassing):
         graph_data.edge_index = update_edges(graph_data.pos, self.config.k_neighbors, self.config.radius)
         node_type_i = graph_data.node_type[graph_data.edge_index[1]]
         node_type_j = graph_data.node_type[graph_data.edge_index[0]]
-        out = self.propagate(edge_index=graph_data.edge_index, x=graph_data.x, pos=graph_data.pos, type_i=node_type_i, type_j=node_type_j, node_type=graph_data.node_type)
+        out = self.propagate(edge_index=graph_data.edge_index, x=graph_data.x, pos=graph_data.pos, node_type_i=node_type_i, node_type_j=node_type_j, node_type=graph_data.node_type)
         graph_data.x[:, :self.config.number_of_base_latent_features] = out
         return graph_data
 
-    def message(self, x_j: torch.Tensor, x_i: torch.Tensor, pos_j: torch.Tensor, pos_i: torch.Tensor, type_i: torch.Tensor, type_j: torch.Tensor) -> torch.Tensor:
+    def message(self, x_j: torch.Tensor, x_i: torch.Tensor, pos_j: torch.Tensor, pos_i: torch.Tensor, node_type_i: torch.Tensor, node_type_j: torch.Tensor) -> torch.Tensor:
         dist = torch.norm(pos_i - pos_j, dim=-1, keepdim=True)
         x_j_base = x_j[:, :self.config.number_of_base_latent_features]
         x_i_base = x_i[:, :self.config.number_of_base_latent_features]
         combined = torch.cat([x_j_base, x_i_base, dist], dim=-1)
-        src_type = type_j.long()
-        dst_type = type_i.long()
+        src_type = node_type_j.squeeze(-1).long()
+        dst_type = node_type_i.squeeze(-1).long()
         messages = torch.zeros(combined.shape[0], self.config.number_of_base_latent_features, device=combined.device)
         for s in range(3):
             for d in range(3):
@@ -104,9 +104,9 @@ class DynamicsGNN(MessagePassing):
         bc_features = x[:, self.config.number_of_base_latent_features : self.config.number_of_base_latent_features + max(self.config.number_of_pressure_bc_latent_features, self.config.number_of_velocities_bc_latent_features)]
         phys_features = x[:, -6:]
 
-        mask_free = (node_type == 0)
-        mask_pressure = (node_type == 1)
-        mask_velocity = (node_type == 2)
+        mask_free = (node_type.squeeze(-1) == 0)
+        mask_pressure = (node_type.squeeze(-1) == 1)
+        mask_velocity = (node_type.squeeze(-1) == 2)
 
         updated_features = aggr_out.clone()
         if mask_pressure.sum() > 0:
